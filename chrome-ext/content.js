4f8bb2ab495d5f94216cbdcec2e7dd19ca407ab5
mhw32 <me@mikewuis.me>
2019-08-24 17:35:21 -0700

    updates

--- chrome-ext/content.js
+++ chrome-ext/content.js
@@ -9,8 +9,8 @@
 //     }
 // );
 
-// Previous dom, that we want to track.
-var prevDOM = null;
+var oldParentDiv, newParentDiv, oldParentChildren, imageIndex, nextChild;
+var prevDOM = null;  // Previous dom, that we want to track.
 var buttonElement = document.createElement("BUTTON");
 var buttonContent = document.createTextNode("Buy");
 buttonElement.setAttribute("id", "app_buy_button");
@@ -30,25 +30,34 @@ document.addEventListener('mousemove', function (e) {
             // the parentElement will be the one we added
             prevDOM.parentElement.removeChild(buttonElement);
         }
+
+        oldParentDiv = srcElement.parentElement;
+        oldParentChildren = [...oldParentDiv.children];
+        imageIndex = oldParentChildren.indexOf(srcElement);
         
         if (srcElement.parentElement.className != "app_wrap_div") {
-            console.log('HERE');
+            if (oldParentChildren.length > imageIndex + 1) {
+                // If more children exist after element...
+                nextChild = oldParentChildren[imageIndex + 1];  // get next child
+            }
             // Need to add an extra DIV
-            var oldParentDiv = srcElement.parentElement;
-            var newParentDiv = document.createElement("DIV");
+            newParentDiv = document.createElement("DIV");
             newParentDiv.setAttribute("class", "app_wrap_div");
             newParentDiv.appendChild(srcElement);
             newParentDiv.appendChild(buttonElement);
-            oldParentDiv.appendChild(newParentDiv);
+            
+            if (oldParentChildren.length > imageIndex + 1) {
+                oldParentDiv.insertBefore(newParentDiv, nextChild);
+            } else {  // Either the last or only child
+                oldParentDiv.appendChild(newParentDiv);
+            }
         } else {
-            // We have already added a DIV... directly append child
+            // We have already added a DIV... directly add child to DIV
             srcElement.parentElement.appendChild(buttonElement)
         }
  
         // The current element is now the previous. So we can remove the class
         // during the next iteration.
         prevDOM = srcElement;
-        console.info(srcElement.currentSrc);
-        console.dir(srcElement);
     }
 }, false);
